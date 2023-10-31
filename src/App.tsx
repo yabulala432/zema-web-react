631c0a28822b652f4ef60d11dc512dc52bd9f45a
Yeabsira Yonas <Yabulala432@gmail.com>
2023-10-31 22:27:32 +0300

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,7 @@
-import { Grid, GridItem, useBreakpointValue } from "@chakra-ui/react";
 import "./App.css";
-import SidebarWithList from "./components/SideBarMain";
-import NavBar from "./components/NavBar";
 
 function App() {
-  const backgroundImage = useBreakpointValue({
-    base: "url('../src/assets/img.png')",
-    lg: "url('../src/assets/cropped_2.webp')",
-  });
-
-  return (
-    <>
-      <Grid
-        bgImage={useBreakpointValue({ base: backgroundImage })}
-        bgSize={"cover"}
-        bgRepeat={'no-repeat'}
-        // height={'100vh'}
-        templateAreas={{
-          base: `"nav  nav" 
-              " aside main"
-              "aside  main"`,
-        }}
-        templateColumns={{
-          base: ".85fr 2fr",
-          lg: ".4fr 2fr",
-        }}
-      >
-        <GridItem area={"aside"} padding={0} margin={0} opacity={1}>
-          <SidebarWithList />
-        </GridItem>
-
-        <GridItem
-          area={"main"}
-        ></GridItem>
-
-        <GridItem
-          bgColor={"blackAlpha.300"}
-          height={"100px"}
-          p={0}
-          m={0}
-          area={"nav"}
-        >
-          <NavBar />
-        </GridItem>
-      </Grid>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
--- src/services/sampleDataFetching.ts
+++
@@ -1,8 +0,0 @@
-import axios, { AxiosRequestConfig } from "axios";
-
-class DataFetch {
-  public static get(): Promise<AxiosRequestConfig> | undefined {
-    return axios.get("http://localhost:3000/posts");
-  }
-}
-export default DataFetch;
