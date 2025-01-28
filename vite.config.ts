1c81dcd12be9b62876ad6dfc28cc165ea3a94ef2
Yeabsira Yonas <Yabulala432@gmail.com>
2025-01-28 07:37:27 +0300

    config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      // Proxy to handle requests to the external Ethiopian Orthodox website
       "/api": {
-        target: "https://www.ethiopianorthodox.org", // Target server
-        changeOrigin: true, // Changes the origin of the host header to the target URL
-        rewrite: (path) => path.replace(/^\/api/, ""), // Removes "/api" prefix from requests
-        secure: true, // Use HTTPS for the proxy
+        target: "https://www.ethiopianorthodox.org",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
-  },
-  build: {
-    target: "esnext",
   },
 });
