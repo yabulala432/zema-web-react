ebdc82936366834c51b37af8faf26d48e5a5cf04
Yeabsira Yonas <Yabulala432@gmail.com>
2025-01-28 05:05:58 +0300

    title

--- src/components/main-screen/ZemaTitle.tsx
+++ src/components/main-screen/ZemaTitle.tsx
@@ -5,8 +5,13 @@ import dataStore from "../../store/DataStore";
 function ZemaTitle() {
   const zemaTtile = dataStore((state) => state.zemaTitle);
   return (
-    <Text fontSize={"20px"} color={"#9d6651"} fontWeight={"bold"}>
-      {zemaTtile}
+    <Text
+      textAlign={"center"}
+      fontSize={"3xl"}
+      color={"#691c08"}
+      fontWeight={"bold"}
+    >
+      {zemaTtile.replace(/_/g, " ").split("-*-")[0]}
     </Text>
   );
 }
