686fe6b1c86f2f4e01dfecb566551066dafeb0b2
Yeabsira Yonas <Yabulala432@gmail.com>
2025-01-28 05:04:24 +0300

    config for pdf fetching

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,18 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Proxy to handle requests to the external Ethiopian Orthodox website
+      "/api": {
+        target: "https://www.ethiopianorthodox.org", // Target server
+        changeOrigin: true, // Changes the origin of the host header to the target URL
+        rewrite: (path) => path.replace(/^\/api/, ""), // Removes "/api" prefix from requests
+        secure: true, // Use HTTPS for the proxy
+      },
+    },
+  },
   build: {
     target: "esnext",
   },
-  // server:{
-  //   watch:{
-  //     usePolling:true,
-  //   }
-  // }
 });
